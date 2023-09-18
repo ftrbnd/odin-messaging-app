239c0c5d56a959fccf1471ff81f3ad97a3848da7
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-09-18 00:06:21 -0700

    Redirect user to home page after signing out

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,7 +1,16 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function SignOutButton() {
-  return <button onClick={() => signOut()}>Sign Out</button>;
+  const router = useRouter();
+
+  const handleClick = async () => {
+    await signOut();
+    router.push('/');
+    router.refresh();
+  };
+
+  return <button onClick={handleClick}>Sign Out</button>;
 }
