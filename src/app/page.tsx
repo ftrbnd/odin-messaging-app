6ef0508734b3086bba9bf1e5a74f655fbef2f5a5
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-09-17 23:35:04 -0700

    Rearrange home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,13 @@ export default async function Home() {
         <div className="hero h-full bg-base-200">
           <div className="hero-content text-center">
             <div className="max-w-md">
-              <h1 className="text-5xl font-bold">Hello there</h1>
+              <h1 className="text-5xl font-bold">Hello there!</h1>
               <p className="py-6">
-                {
-                  "This messaging app is a project that was created following The Odin Project's NodeJS course. I built this with: TypeScript, Next.js, TailwindCSS, DaisyUI, MongoDB/Mongoose, and NextAuth."
-                }
+                This messaging app is a project that I created as I followed{' '}
+                <a className="link link-primary" href="https://www.theodinproject.com/lessons/nodejs-messaging-app" target="_blank">
+                  {"The Odin Project's"}
+                </a>{' '}
+                NodeJS course. I built this with Next.js/TypeScript, Tailwind CSS/DaisyUI, MongoDB/Mongoose, and NextAuth.
               </p>
               <Link href={'/signin'}>
                 <button className="btn btn-primary">Get Started</button>
